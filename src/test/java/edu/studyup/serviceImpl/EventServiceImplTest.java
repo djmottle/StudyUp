765f85c755d412ddb6c099da5af8db5117d291d4
djmottle <djmottle@ucdavis.edu>
2019-02-20 11:08:02 -0800

    Fixed 2 bugs

--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -62,10 +62,26 @@ class EventServiceImplTest {
 	}
 
 	@Test
-	void testUpdateEventName_GoodCase() throws StudyUpException {
+	void testUpdateEvent_nameWith20Char_goodCase() throws StudyUpException {
 		int eventID = 1;
-		eventServiceImpl.updateEventName(eventID, "Renamed Event 1");
-		assertEquals("Renamed Event 1", DataStorage.eventData.get(eventID).getName());
+		assertEquals("This has exactly 20." ,eventServiceImpl.updateEventName(eventID, "This has exactly 20.").getName());
+	}
+	
+	
+	@Test
+	void testUpdateEvent_nameTooLong_badCase() throws StudyUpException {
+		int eventID = 1;
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.updateEventName(eventID, "Wow this is a really long name, it should probably be shorter than this.");
+		  });
+	}
+	
+	//
+	@Test
+	void testUpdateEventName_LessThan20_goodCase() throws StudyUpException {
+		int eventID = 1;
+		eventServiceImpl.updateEventName(eventID, "Event name");
+		assertEquals("Event name", DataStorage.eventData.get(eventID).getName());
 	}
 	
 	@Test
@@ -76,4 +92,111 @@ class EventServiceImplTest {
 		  });
 	}
 	
+	@Test
+	//Sets the date of the only event to a past date and checks if active events is empty.
+	void testGetActiveEvents_NoActiveEvents() {
+		int eventID = 1;
+		@SuppressWarnings("deprecation")
+		Date pastDate = new Date(100,0, 1);
+		DataStorage.eventData.get(eventID).setDate(pastDate);
+		List<Event> activeEvents = eventServiceImpl.getActiveEvents();
+		assertTrue(activeEvents.isEmpty());
+	}
+
+	
+	@Test
+	//Sets the date of the only event to a future date and checks if active events returns the event.
+	void testGetActiveEvents_ActiveEvent() {
+		int eventID = 1;
+		@SuppressWarnings("deprecation")
+		Date futureDate = new Date(3000, 0, 1);
+		DataStorage.eventData.get(eventID).setDate(futureDate);
+		List<Event> expectedEvents = new ArrayList<>();
+		expectedEvents.add(DataStorage.eventData.get(eventID));
+		List<Event> activeEvents = eventServiceImpl.getActiveEvents();
+		assertEquals(expectedEvents, activeEvents);
+	}
+	
+	@Test
+	//Sets the date of the only event to a past date and checks if past events returns the event.
+	void testGetPastEvents_PastEvent() {
+		int eventID = 1;
+		@SuppressWarnings("deprecation")
+		Date pastDate = new Date(100, 0, 1);
+		DataStorage.eventData.get(eventID).setDate(pastDate);
+		List<Event> expectedEvents = new ArrayList<>();
+		expectedEvents.add(DataStorage.eventData.get(eventID));
+		List<Event> pastEvents = eventServiceImpl.getPastEvents();
+		assertEquals(expectedEvents, pastEvents);
+	}	
+	
+	@Test
+	//Sets the date of the only event to a future date and checks if past events returns the event.
+	void testGetPastEvents_FutureEvent() {
+		int eventID = 1;
+		@SuppressWarnings("deprecation")
+		Date futureDate = new Date(2000, 0, 1);
+		DataStorage.eventData.get(eventID).setDate(futureDate);
+		List<Event> expectedEvents = new ArrayList<>();
+		expectedEvents.add(DataStorage.eventData.get(eventID));
+		List<Event> futureEvents = eventServiceImpl.getPastEvents();
+		assertTrue(futureEvents.isEmpty());
+	}		
+	@Test
+	void testAddStudentToEvent_nullEvent_badCase() {
+		int eventID = 3; //Event does not exist in DataStorage
+		Student studentToAdd = new Student();
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.addStudentToEvent(studentToAdd, eventID);
+		  });
+	}
+	
+	@Test
+	void testAddStudentToEvent_startsWithNoStudents_goodCase() throws StudyUpException {
+		int eventID = 1;
+		Student studentToAdd = new Student();
+		List<Student> expectedStudentList = new ArrayList<>();
+		expectedStudentList.add(studentToAdd);
+		DataStorage.eventData.get(eventID).setStudents(null);
+		
+		assertEquals(expectedStudentList, eventServiceImpl.addStudentToEvent(studentToAdd, eventID).getStudents());
+	}
+	
+	@Test
+	void testAddStudentToEvent_startsWithStudent_goodCase() throws StudyUpException {
+		int eventID = 1;
+		Student studentToAdd = new Student();
+		List<Student> expectedStudentList = DataStorage.eventData.get(eventID).getStudents();
+		expectedStudentList.add(studentToAdd);
+		assertEquals(expectedStudentList, eventServiceImpl.addStudentToEvent(studentToAdd, eventID).getStudents());
+	}
+	
+	/*
+	 * An Event should have no more than 2 students, so adding more should
+	 * throw an exception
+	 */
+	@Test
+	void testAddStudentToEvent_addThreeStudents_badCase() throws StudyUpException {
+		int eventID = 1;
+		int maxStudents = 2;
+		Student studentToAdd = new Student();
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			for(int i = 0; i != maxStudents + 1; ++i) {
+			eventServiceImpl.addStudentToEvent(studentToAdd, eventID);
+			}
+		  });
+	}
+	
+	@Test
+	//Sets the date of the only event to a future date and checks if past events returns the event.
+	void testDeleteEvent() {
+		int eventID = 1;
+		@SuppressWarnings("deprecation")
+		Date pastDate = new Date(100, 0, 1);
+		DataStorage.eventData.get(eventID).setDate(pastDate);
+		List<Event> expectedEvents = new ArrayList<>();
+		expectedEvents.add(DataStorage.eventData.get(eventID));
+		Event deletedEvent = eventServiceImpl.deleteEvent(eventID);
+		assertEquals(expectedEvents, deletedEvent);
+	}
 }
